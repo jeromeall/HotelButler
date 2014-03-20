1b93298568c95f4bfbe2a59807041ab48f886f66
jeromeall <jeromeallouche01@gmail.com>
2014-03-20 11:55:12 -0700

    first commit - added secret key base, and updated .gitignore file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HotelButler::Application.config.secret_key_base = '4cbc1bdc05648ac0cd32d4e823d93608f359455582852ced842515a9bd29ec9346df150a1441f24d02645a2c5e37c1aaa45de150e2cfb494955d0f9a1d1aff3b'
