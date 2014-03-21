087a52682daab5d3b21a845bb133b336f26f2fdd
jeromeall <jeromeallouche01@gmail.com>
2014-03-20 17:43:13 -0700

    index page is erb instead of angular, with links to sign up and sign in, and which then redirect to stays index (not created yet), which will be an angular spa

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -3,10 +3,10 @@ var hotelButlerRouter = angular.module("hotelButlerRouter", ["ngRoute"]);
 
 
 hotelButlerRouter.config(['$routeProvider',function($routeProvider){
-  $routeProvider.
-    when("/",
-    {
-      templateUrl: "templates/index.html.erb",
-      controller: "siteCtrl"
-    });
+  // $routeProvider.
+  //   when("/",
+  //   {
+  //     templateUrl: "templates/index.html.erb",
+  //     controller: "siteCtrl"
+  //   });
 }]);
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 HotelButler::Application.routes.draw do
   devise_for :guests
   root "sites#index"
+  resources :sites
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
