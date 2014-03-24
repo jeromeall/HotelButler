887d885bef61df0c98130b075ef992c8e6aeccf1
jeromeall <jeromeallouche01@gmail.com>
2014-03-24 15:50:06 -0700

    rerouting spa to application index method - stays controller will be api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def index
+    render "stays/index"
+  end
+
 protected
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || stays_path
+    request.env['omniauth.origin'] || stored_location_for(resource) || home_path
   end
 
 end
--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,4 +1,8 @@
 class SitesController < ApplicationController
   def index
+    render :layout => 'application', :nothing => true
+  end
+
+  def home
   end
 end
