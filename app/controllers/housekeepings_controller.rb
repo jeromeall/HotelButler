149b4da3b97f4c1440e5efe5005acef86139154d
jeromeall <jeromeallouche01@gmail.com>
2014-03-24 23:30:58 -0700

    added controller methods for houseekings

--- app/controllers/housekeepings_controller.rb
+++ app/controllers/housekeepings_controller.rb
@@ -1,4 +1,21 @@
 class HousekeepingsController < ApplicationController
 respond_to :json
 
+def index
+  render json: Housekeeping.where(stay_id: params[:stay_id])
+end
+
+def create
+  stay = Stay.find(params[:stay_id])
+  housekeeping = stay.housekeepings.new(safe_params)
+  render json: housekeeping
+end
+
+def safe_params
+  params.require(:housekeeping).permit(:request)
+end
+
+
+
+
 end
