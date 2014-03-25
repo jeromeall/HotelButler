efb5f88473291078995ff2f77e763fa3df78a608
jeromeall <jeromeallouche01@gmail.com>
2014-03-24 23:38:35 -0700

    added view for housekeeping request form, route, and link in mainmenu

--- app/controllers/housekeepings_controller.rb
+++ app/controllers/housekeepings_controller.rb
@@ -1,21 +1,19 @@
 class HousekeepingsController < ApplicationController
-respond_to :json
+  respond_to :json
 
-def index
-  render json: Housekeeping.where(stay_id: params[:stay_id])
-end
+  def index
+    render json: Housekeeping.where(stay_id: params[:stay_id])
+  end
 
-def create
-  stay = Stay.find(params[:stay_id])
-  housekeeping = stay.housekeepings.new(safe_params)
-  render json: housekeeping
-end
+  def create
+    stay = Stay.find(params[:stay_id])
+    housekeeping = stay.housekeepings.new(safe_params)
+    render json: housekeeping
+  end
 
-def safe_params
-  params.require(:housekeeping).permit(:request)
-end
-
-
+  def safe_params
+    params.require(:housekeeping).permit(:request)
+  end
 
 
 end
