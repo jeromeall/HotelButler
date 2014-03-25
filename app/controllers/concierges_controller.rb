8c0836520ca79ad902ef9ae2ff819ee0bef477b3
jeromeall <jeromeallouche01@gmail.com>
2014-03-24 19:17:56 -0700

    created concierge controller method for create

--- app/controllers/concierges_controller.rb
+++ app/controllers/concierges_controller.rb
@@ -5,4 +5,14 @@ def index
   render json: Concierge.where(stay_id: params[:stay_id])
 end
 
+def create
+  stay = Stay.find(params[:stay_id])
+  concierge = stay.concierges.new(safe_params)
+  render json: concierge
+end
+
+def safe_params
+  params.require(:concierge).permit(:event, :num_party, :event_date)
+end
+
 end
