6164c0fbb2c2ba871eb4089423adee86cea79712
jeromeall <jeromeallouche01@gmail.com>
2014-03-25 00:16:49 -0700

    concierge request saving to server

--- app/controllers/concierges_controller.rb
+++ app/controllers/concierges_controller.rb
@@ -7,12 +7,12 @@ end
 
 def create
   stay = Stay.find(params[:stay_id])
-  concierge = stay.concierges.new(safe_params)
+  concierge = stay.concierges.create(safe_params)
   render json: concierge
 end
 
 def safe_params
-  params.require(:concierge).permit(:event, :num_party, :event_date)
+  params.require(:concierge).permit(:event_type, :num_party, :event_date, :details)
 end
 
 end
