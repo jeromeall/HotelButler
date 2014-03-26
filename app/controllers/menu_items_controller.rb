d5fd66758bb4e21241a2b6190d9fb1659af41110
jeromeall <jeromeallouche01@gmail.com>
2014-03-26 13:55:40 -0700

    changed routes, services for menu_items to show up based on the stay_id

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -2,7 +2,7 @@ class MenuItemsController < ApplicationController
   respond_to :json
 
   def index
-    render json: MenuItem.where(hotel_id: params[:hotel_id]).sort
+    render json: MenuItem.where(hotel_id: Room.find(Stay.find(params[:stay_id].to_i).room_id).hotel_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ HotelButler::Application.routes.draw do
   devise_for :guests
   root to: "sites#index"
   get "/home", to: "application#index", as: "home"
-  resources :hotels, :defaults => {format: :json} do 
-    resources :menu_items, :defaults => {format: :json}
-  end
+  resources :hotels, :defaults => {format: :json}
+
 
   resources :stays, :defaults => {format: :json} do
     resources :housekeepings, :defaults => {format: :json}
     resources :concierges, :defaults => {format: :json}
     resources :roomservices, :defaults => {format: :json}
+    resources :menu_items, :defaults => {format: :json}
   end
   
 
