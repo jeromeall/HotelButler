d60f689cb78985c142b38fd96f6065a7ad9f4284
jeromeall <jeromeallouche01@gmail.com>
2014-03-26 13:24:13 -0700

    generated angular roomserviceCreateCtrl

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -2,7 +2,7 @@ class MenuItemsController < ApplicationController
   respond_to :json
 
   def index
-    render json: MenuItem.where(hotel_id: params[:hotel_id])
+    render json: MenuItem.where(hotel_id: params[:hotel_id]).sort
   end
 
 end
