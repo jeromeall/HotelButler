c0ca6ee357976fd5ae6a7d004b05fbe2861b1123
jeromeall <jeromeallouche01@gmail.com>
2014-03-27 16:34:08 -0700

    concierge redirects to show page with reservation details

--- app/controllers/concierges_controller.rb
+++ app/controllers/concierges_controller.rb
@@ -11,8 +11,17 @@ def create
   render json: concierge
 end
 
+def show
+  stay = Stay.find(params[:stay_id])
+  concierge = stay.concierges.find(params[:id])
+  render json: concierge
+end
+
+
 def safe_params
   params.require(:concierge).permit(:event_type, :num_party, :event_date, :details)
 end
+
+
 
 end
