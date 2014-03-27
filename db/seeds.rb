f920a8c6ef0635205cebf7491225f32bbb025bc7
jeromeall <jeromeallouche01@gmail.com>
2014-03-27 16:51:49 -0700

    more seed - menu items

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
+  # Seed Hotels
   wynn = Hotel.create(name: "Wynn", num_rooms: 500, city: "Las Vegas", state: "NV")
   mResort = Hotel.create(name: "M Resort", num_rooms: 300, city: "Las Vegas", state: "NV")
   theEncore = Hotel.create(name: "The Encore", num_rooms: 400, city: "Las Vegas", state: "NV")
 
+
+
+  # Seed Rooms
   wynn.rooms.create(room_num: 101, vacant_stat: true)
   wynn.rooms.create(room_num: 102, vacant_stat: true)
   wynn.rooms.create(room_num: 103, vacant_stat: true)
@@ -43,3 +48,18 @@
   theEncore.rooms.create(room_num: 109, vacant_stat: true)
   theEncore.rooms.create(room_num: 110, vacant_stat: true)
   
+  # Seed Menu Items
+  wynn.menu_items.create(name: "Steak", description: "20oz Ribeye with pepper sauce and served with fries", price: "30.00")
+  wynn.menu_items.create(name: "Grilled Salmon", description: "Healthy grilled Salmon with sauteed vegetables", price: "27.00")
+  wynn.menu_items.create(name: "Spaghetti with Meatballs", description: "Delicious spaghetti with homemade sauce and meatballs", price: "24.00")
+  wynn.menu_items.create(name: "Pancakes", description: "Stack of 3 pancakes with maple syrup and whipped cream", price: "20.00")
+  
+  mResort.menu_items.create(name: "Steak", description: "20oz Ribeye with pepper sauce and served with fries", price: "30.00")
+  mResort.menu_items.create(name: "Grilled Salmon", description: "Healthy grilled Salmon with sauteed vegetables", price: "27.00")
+  mResort.menu_items.create(name: "Spaghetti with Meatballs", description: "Delicious spaghetti with homemade sauce and meatballs", price: "24.00")
+  mResort.menu_items.create(name: "Pancakes", description: "Stack of 3 pancakes with maple syrup and whipped cream", price: "20.00")
+
+  theEncore.menu_items.create(name: "Steak", description: "20oz Ribeye with pepper sauce and served with fries", price: "30.00")
+  theEncore.menu_items.create(name: "Grilled Salmon", description: "Healthy grilled Salmon with sauteed vegetables", price: "27.00")
+  theEncore.menu_items.create(name: "Spaghetti with Meatballs", description: "Delicious spaghetti with homemade sauce and meatballs", price: "24.00")
+  theEncore.menu_items.create(name: "Pancakes", description: "Stack of 3 pancakes with maple syrup and whipped cream", price: "20.00")
