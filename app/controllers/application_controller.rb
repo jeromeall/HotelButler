3961902a2bc57b604fcb0fe558348f99420bab80
jeromeall <jeromeallouche01@gmail.com>
2014-03-27 14:55:53 -0700

    changed devise login routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 protected
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || home_path
+    request.env['omniauth.origin'] || stored_location_for(resource) || authenticated_path
   end
 
 end
