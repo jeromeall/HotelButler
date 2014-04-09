349d05684e4ecf02b1b9ee20f6f1caae159ab77e
jeromeall <jeromeallouche01@gmail.com>
2014-04-09 14:28:13 -0700

    working on stays index layout

--- app/models/stay.rb
+++ app/models/stay.rb
@@ -5,6 +5,13 @@ class Stay < ActiveRecord::Base
   has_many :concierges
   has_many :roomservices
 
+  validate :checkout_cannot_be_after_checkin
+
+  def checkout_cannot_be_after_checkin
+    if self.checkout_date < self.checkin_date
+      errors.add(:checkout_date, "checkout date cannot be after checkin date.")
+    end
+  end
   def self.guest_stays(guest)
     return guest.stays
   end
