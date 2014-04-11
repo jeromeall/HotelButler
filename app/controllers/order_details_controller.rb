199d4bdd804bc408a8c9514e4b2990792d4adf6c
jeromeall <jeromeallouche01@gmail.com>
2014-04-10 17:04:27 -0700

    Order details not saving properly with roomservice_id - troubleshooting

--- app/controllers/order_details_controller.rb
+++ app/controllers/order_details_controller.rb
@@ -5,7 +5,7 @@ def index
   stay = Stay.find(params[:stay_id])
   roomservice = stay.roomservices.find(params["roomservice_id"])
   order_details = roomservice.order_details
-  render json: order_details, :include => 
+  render json: order_details
 end
 
 def create
