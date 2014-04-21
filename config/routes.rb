3f98813c4f1792d7e93def46e2bd6429c1522e22
jeromeall <jeromeallouche01@gmail.com>
2014-04-21 14:47:54 -0700

    commented routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 HotelButler::Application.routes.draw do
 
   devise_for :guests
+
+  # Root to welcome screen if not signed in
   unauthenticated :guest do
     root to: "sites#index", :as => "unauthenticated"
   end 
+
+  # Root to Stays index in Angular SPA if authenticated
   authenticated :guest do
     root to: "application#index", :as => "authenticated"
   end 
